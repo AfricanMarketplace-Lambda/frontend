05e91d8c522e24e5d405277b4a5e6fadf52543bb
Chaz Carbis <chazcarbis@gmail.com>
2021-02-01 14:48:29 -0500

    Made the basic scaffold for landing page and item cards. Ready to work on registeration form component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+
+function itemMaker(raw){
+  const data = raw.data
+  return(
+   <div>
+     <img src= {data.picture} alt= {data.title}/>
+     <h3>{data.title}</h3>
+     <p>{data.price}</p>
+     <p>{data.description}</p>
+   </div> 
+ )
+}
+
+
+
+axios
+    .get(`https://tt17-african-marketplace.herokuapp.com/items`)
+    .then(res => {
+      res.data.forEach(document.querySelector('.itemCard').appendChild(itemMaker(res)))
+      })
+     .catch(err => console.log(err))
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to African Marketplace!</h1>
+      <nav>
+        <button>Not a member? Sign Up</button>
+        <input type="text" placeholder= 'Username'/>
+        <input type="text" placeholder= "Password"/> 
+        <button>Sign In</button> 
+      </nav>
+      <div className= 'itemCard'>
+      </div>
+    </div>
+
     </div>
   );
 }
