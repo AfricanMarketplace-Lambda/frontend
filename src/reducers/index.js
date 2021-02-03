b138c1488db91030fa468ed4357a4e4fc54182ba
Michelle Chen <mchen0062@gmail.com>
2021-02-02 20:38:15 -0800

    created actions for fetching data and ItemForm component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,36 @@
-//import {actions} from '../actions';
+import {
+  FETCH_ITEMS_FAIL,
+  FETCH_ITEMS_SUCCESS,
+  FETCH_ITEMS_START,
+} from "../actions";
 
 export const initialState = {
-    // different states here 
-}
+  isFetching: false,
+  items: [],
+  error: "",
+};
 
-export const reducer = ( state = initialState, action) => {
-    switch (action.type){
-        // different action cases here
-        default: 
-        state
-    }
-}
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_ITEMS_START:
+      return {
+        ...state,
+        isFetching: true,
+        items: [],
+      };
+    case FETCH_ITEMS_SUCCESS:
+      return {
+        ...state,
+        items: action.payload,
+      };
+    case FETCH_ITEMS_FAIL:
+      return {
+        ...state,
+        error: "error in the reducer",
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer; 
