5535b053e56494c626a20b82bb3ad25cb2da8615
Michelle Chen <68928523+MichelleCCodes@users.noreply.github.com>
2021-02-03 21:26:43 -0800

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -34,7 +34,7 @@ export const addItem = (item) => (dispatch) =>{
 };
 
 export const updateItem = (item) => (dispatch) =>{
-    axiosWithAuth().put(`api/items/${item.id}`, item)
+    axiosWithAuth().put(`/api/items/${item.id}`, item)
     .then((res) =>{
         console.log('in updateItem', res.data)
         dispatch({type:EDIT_ITEM_SUCCESS, payload: res.data})
